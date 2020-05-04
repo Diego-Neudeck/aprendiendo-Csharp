7ac652b52c6ca0ce879ccdb65867a4752eb67988
Diego-Neudeck <neudeck004@gmail.com>
2020-05-04 17:32:37 -0300

    muestro una resta con WriteLine

--- Ejemplo1/Program.cs
+++ Ejemplo1/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
-
-namespace Ejemplo1
+﻿class MainClass
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public static void Main(string[] args)
+	{
+		int dato1, dato2, resultado;
+		dato1 = 10;
+		dato2 = 20;
+		resultado = dato1 - (dato2 / dato1);
+		//	System.Console.WriteLine ("Hello World!");
+		System.Console.WriteLine("el resultado es: {0}-({1}/{0})={2}", dato1, dato2, resultado);
+	}
 }
