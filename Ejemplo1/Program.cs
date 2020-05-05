ee09611e8f55196ae39c4e8f88105cb7a8770ef2
Diego-Neudeck <neudeck004@gmail.com>
2020-05-05 00:06:51 -0300

    Agrego menu switch para llamar a las funciones.

--- Ejemplo1/Program.cs
+++ Ejemplo1/Program.cs
@@ -3,33 +3,46 @@ using System.Diagnostics.CodeAnalysis;
 
 class MainClass
 {
-	public static void Sumar(double x, double y)
+	public static void Sumar(double x, double y,double z)
 	{
 		double resultado = 0;
-		resultado = x + y;
+		resultado = x + y + z;
+		System.Console.WriteLine();
 		System.Console.WriteLine("la suma es: " + resultado);
 	}
 	public static void exprecion(double a, double b, double c)
 	{
 		double resultado=0;
 		resultado = ((b * b) - (4 * a * c)) / (2 * a);
+		System.Console.WriteLine();
 		System.Console.WriteLine("La expresion es: " + resultado);
-
 	}
 	public static void Main(string[] args)
 	{
 		string selec;
 		double a = 4, b = 3, c = 10; 
-		System.Console.WriteLine("Ingrese opcion 1 para suma, de lo contrario mostrara exprecion");
-		selec = System.Console.ReadLine();
-		if(selec == "1")
+		do
 		{
-			Sumar(a,b); 
-		}
-		else
-		{
-			exprecion(a, b, c);
-		}
-	
+			System.Console.WriteLine("1-Suma a + b + c");
+			System.Console.WriteLine("2-Se realiza (b^2-4*a*c)/(2*a)");
+			System.Console.WriteLine("s-Salir");
+			selec = System.Console.ReadLine();
+			switch (selec)
+			{
+				case "1":
+					Sumar(a,b,c);
+					break;
+				case "2":
+					exprecion(a, b, c);
+					break;
+				/*default:
+					{
+						if(selec!="s") 
+						System.Console.WriteLine("Digito incorrecto");
+					}*/
+			}
+			System.Console.WriteLine("--------------------------");
+		} while (selec!="s");
+		System.Console.WriteLine("Programa finalizado");
 	}
 }
