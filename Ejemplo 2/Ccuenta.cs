86858e54f7f0726fa08f5304d0535d9c87d3ea00
Diego-Neudeck <neudeck004@gmail.com>
2020-05-11 15:59:26 -0300

    Creando proyectos

---
+++ Ejemplo 2/Ccuenta.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejemplo_2
+{
+    class Ccuenta
+    {
+
+
+
+        //atributos
+        private string nombre;
+        private string cuenta;
+        private double saldo;
+        private double tipoDeInteres;
+
+        //propiedades y metodos
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                if (value.Length == 0)
+                {
+                    System.Console.WriteLine("ERROR, nombre no valido");
+                    return;
+                }
+                nombre = value;
+            }
+        }
+
+
+        public string Cuenta
+        {
+            get { return cuenta; }
+            set
+            {
+                if (value.Length == 0)
+                {
+                    System.Console.WriteLine("Error, cuenta no valida");
+                    return;
+                }
+                cuenta = value;
+            }
+        }
+        public double Saldo
+        {
+            get { return saldo; }
+        }
+
+
+        public void Ingreso(double cantidad)
+        {
+            if (cantidad < 0)
+            {
+                System.Console.WriteLine("Error, cantidad negativa");
+                return;
+            }
+            saldo = saldo + cantidad;
+        }
+
+        public void Reintegro(double cantidad)
+        {
+            if (saldo - cantidad < 0)
+            {
+                System.Console.WriteLine("Error, no dispone de saldo");
+                return;
+            }
+            saldo = saldo - cantidad;
+        }
+
+
+        public double TipoDeInteres
+        {
+            get { return tipoDeInteres; }
+            set
+            {
+                if (value < 0)
+                {
+                    System.Console.WriteLine("Error, tipo no valido");
+                    return;
+                }
+                tipoDeInteres = value;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Ejemplo 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejemplo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ccuenta cuenta01 = new Ccuenta();
+            cuenta01.Nombre = "un nombre";
+            cuenta01.Cuenta = "una cuenta";
+            cuenta01.TipoDeInteres = 2.5;
+
+            cuenta01.Ingreso(12000);
+            cuenta01.Reintegro(3000);
+
+            System.Console.WriteLine(cuenta01.Nombre);
+            System.Console.WriteLine(cuenta01.Cuenta);
+            System.Console.WriteLine(cuenta01.Saldo);
+            System.Console.WriteLine(cuenta01.TipoDeInteres);
+        }
+    }
+}
