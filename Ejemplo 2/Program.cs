d789729664b53e0c670be15e8d01c25355f4fab5
Diego-Neudeck <neudeck004@gmail.com>
2020-05-11 19:54:24 -0300

    Agrego nombres.

--- Ejemplo 2/Program.cs
+++ Ejemplo 2/Program.cs
@@ -7,17 +7,24 @@ namespace Ejemplo_2
         static void Main(string[] args)
         {
             Ccuenta cuenta01 = new Ccuenta();
-            cuenta01.Nombre = "un nombre";
-            cuenta01.Cuenta = "una cuenta";
-            cuenta01.TipoDeInteres = 2.5;
+            System.Console.WriteLine("Ingrese nombre: ");
+            cuenta01.Nombre = System.Console.ReadLine();
+            System.Console.WriteLine("Ingrese Cuenta: ");
+            cuenta01.Cuenta = System.Console.ReadLine();
+            System.Console.WriteLine("Ingrese el interes: ");
+            cuenta01.TipoDeInteres = Convert.ToDouble(Console.ReadLine());
 
-            cuenta01.Ingreso(12000);
-            cuenta01.Reintegro(3000);
+            System.Console.WriteLine("Introduzca ingreso:");
+            cuenta01.Ingreso(Convert.ToDouble(Console.ReadLine()));
+            System.Console.WriteLine("Introduzca reintegro: ");
+            cuenta01.Reintegro(Convert.ToDouble(Console.ReadLine()));
 
-            System.Console.WriteLine(cuenta01.Nombre);
-            System.Console.WriteLine(cuenta01.Cuenta);
-            System.Console.WriteLine(cuenta01.Saldo);
-            System.Console.WriteLine(cuenta01.TipoDeInteres);
+            System.Console.WriteLine("------------------------------");
+
+            System.Console.WriteLine("Nombre cuenta: " + cuenta01.Nombre);
+            System.Console.WriteLine("Cuenta: " + cuenta01.Cuenta);
+            System.Console.WriteLine("Saldo: " +cuenta01.Saldo);
+            System.Console.WriteLine("Interes: " + cuenta01.TipoDeInteres);
         }
     }
 }
